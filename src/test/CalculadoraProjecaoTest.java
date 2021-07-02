e580e7556086db434729b8e7e9644f16f5b89564
Natasha <natashabusnardo@gmail.com>
2021-07-02 14:59:09 -0300

    Teste da classe CalculadoraProjecao

---
+++ src/test/CalculadoraProjecaoTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import DTO.CalculadoraProjecao;
+
+public class CalculadoraProjecaoTest {
+
+    @Test
+    public void test() {
+        CalculadoraProjecao calc = new CalculadoraProjecao();
+
+        int output_f = calc.projecao(2, true);
+
+        //teste de saída
+        assertEquals(60, output_f);
+    }
+
+}
+
